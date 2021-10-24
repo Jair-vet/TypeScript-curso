f43a4121bfd1eb6428b4ef1bc8086218e2a82b71
Jair <jairaceves56@gmail.com>
2021-10-24 18:10:57 -0500

    Trabajando con objetos en Ts

---
+++ objetos/objects.ts
@@ -0,0 +1,29 @@
+(() => {
+
+    let flash: { name: string, age?: number, powers: string[], getName?: () => string } =  {
+        name: 'Barry Allen',
+        age: 24,
+        powers: ['Súper velocidad', 'Viajar en el tiempo']
+    }
+
+    let superman: { name: string, age?: number, powers: string[], getName?: () => string } =  {
+        name: 'Clark Kent',
+        age: 77,
+        powers: ['Súper velocidad']
+    }
+
+
+    flash = {
+        name: 'Clark Kent',
+        // age: 70,
+        powers: ['Súper fuerza'],
+        getName(){
+            return this.name;
+        }
+    }
+
+    console.log( flash.getName!() )
+    
+
+
+})()
