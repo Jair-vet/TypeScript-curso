82b0c7af5abfd68eaf18811cfeb86cca5883e363
Jair <jairaceves56@gmail.com>
2021-10-24 18:47:21 -0500

    Trabajando con let-var en Ts

--- ES6/let-var.ts
+++ ES6/let-var.ts
@@ -1,6 +1,14 @@
 (() => {
 
-    console.log('let');
+    const nombre = 'Jair';
+
+    const getName = () => {
+        console.log('Viejo getName');
+        
+    }
+
     
+    // getName = () => { console.log( 'Nuevo getName')};
+    // getName()
 
 })()
