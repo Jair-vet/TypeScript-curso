7910767dc9087267ce91f40958330657ccd220c3
Jair <jairaceves56@gmail.com>
2021-10-24 18:15:30 -0500

    Primeros ejercicios con funciones en TS

---
+++ funciones/functions.ts
@@ -0,0 +1,21 @@
+(() => {
+
+    const hero: string = 'Flash';
+
+    function returnName():string{
+        return hero;
+    }
+
+    const activtebatisignal = ():string => {
+        return 'Batiseñal Activada';
+    }
+
+    console.log(typeof activtebatisignal); // Nos indica el tipo que es
+    
+    const heroName = returnName();
+    
+
+
+
+})()
+
