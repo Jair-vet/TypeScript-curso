dba0d890f25200ce0096ecbd49ae01240aea3f93
Jair <jairaceves56@gmail.com>
2021-10-26 16:54:35 -0500

    Interface basica en Ts

--- interfaces/basic.ts
+++ interfaces/basic.ts
@@ -1,17 +1,27 @@
-"use strict";
 (() => {
-    let flash = {
+
+    interface Heroe {
+        name: string,
+        age?: number,
+        powers: string[],
+        getName?: () => string;
+    }
+
+    let flash: Heroe =  {
         name: 'Barry Allen',
         age: 24,
         powers: ['Súper velocidad', 'Viajar en el tiempo']
-    };
-    let superman = {
+    }
+    
+    let superman: Heroe =  {
         name: 'Clark Kent',
         age: 77,
         powers: ['Súper velocidad'],
         getName() {
             return this.name;
         }
-    };
-})();
-//# sourceMappingURL=main.js.map
+    }
+    
+
+
+})()
