1acb7f4011f188ffb5cfa0ff350e1271247e85fa
Jair <jairaceves56@gmail.com>
2021-10-27 14:11:03 -0500

    Implementacion de interfaces usando Clases en Ts

---
+++ interfaces/classes.ts
@@ -0,0 +1,34 @@
+(() => {
+
+    interface Xmen {
+        name: string;
+        realName: string;
+        mutantPower( id: number ):string;
+    }
+
+    interface Human {
+        age: number;
+    }
+
+    class Mutant implements Xmen, Human {
+
+        public constructor(
+            public age: number,
+            public name : string,
+            public realName: string,
+         ){}
+
+         
+        mutantPower( id: number ){
+            return this.name + ' ' + this.realName;
+        }
+    }
+
+    //Crear instancia de la clase Mutant que implementa de dos interfaces
+    const villian1 = new Mutant(32, 'Dazzler', 'Alison Blaire');
+
+    console.log(villian1);
+    //console.log(villian1.mutantPower(1));
+
+
+})()
--- main.js
+++ main.js
@@ -15,6 +15,20 @@
     };
 })();
 (() => {
+    class Mutant {
+        constructor(age, name, realName) {
+            this.age = age;
+            this.name = name;
+            this.realName = realName;
+        }
+        mutantPower(id) {
+            return this.name + ' ' + this.realName;
+        }
+    }
+    const villian1 = new Mutant(32, 'Dazzler', 'Alison Blaire');
+    console.log(villian1);
+})();
+(() => {
     const client = {
         name: 'Carlos',
         age: 21,
