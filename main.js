8bb5f73c543ea80a21fc8bf778a512b5a9343f1f
Jair <jairaceves56@gmail.com>
2021-10-27 15:13:16 -0500

    Creacion del namespace y primer uso en Ts

--- main.js
+++ main.js
@@ -1,99 +1,14 @@
 "use strict";
-const conducirBatimovil = (auto) => {
-    auto.encender = true;
-    auto.velocidadMaxima = 100;
-    auto.acelear();
-};
-const batimovil = {
-    encender: false,
-    velocidadMaxima: 0,
-    acelear() {
-        console.log("...... gogogo!!!");
-    }
-};
-const guason = {
-    reir: true,
-    comer: true,
-    llorar: false
-};
-const reir = (guason) => {
-    if (guason.reir) {
-        console.log("JAJAJAJA");
-    }
-};
-const ciudadGotica = (ciudadanos) => {
-    return ciudadanos.length;
-};
-class Persona {
-    constructor(nombre, edad, sexo, estadoCivil) {
-        this.nombre = nombre;
-        this.edad = edad;
-        this.sexo = sexo;
-        this.estadoCivil = estadoCivil;
-    }
-    imprimirBio() {
-    }
-}
-const persona1 = new Persona('Juan', 43, 'Hombre', 'Soltero');
-console.log(persona1);
-(() => {
-    let flash = {
-        name: 'Barry Allen',
-        age: 24,
-        powers: ['Súper velocidad', 'Viajar en el tiempo']
+var Validations;
+(function (Validations) {
+    Validations.validateText = (text) => {
+        return (text.length > 3) ? true : false;
     };
-    let superman = {
-        name: 'Clark Kent',
-        age: 77,
-        powers: ['Súper velocidad'],
-        getName() {
-            return this.name;
-        }
+    Validations.validateDate = (myDate) => {
+        return (isNaN(myDate.valueOf()))
+            ? false
+            : true;
     };
-})();
-(() => {
-    class Mutant {
-        constructor(age, name, realName) {
-            this.age = age;
-            this.name = name;
-            this.realName = realName;
-        }
-        mutantPower(id) {
-            return this.name + ' ' + this.realName;
-        }
-    }
-    const villian1 = new Mutant(32, 'Dazzler', 'Alison Blaire');
-})();
-(() => {
-    const client = {
-        name: 'Carlos',
-        age: 21,
-        address: {
-            id: 1233,
-            zip: 'KTY-294',
-            city: 'Ottawa',
-        },
-        getFullAdress(id) {
-            return this.address.city;
-        }
-    };
-    const client2 = {
-        name: 'Melissa',
-        age: 43,
-        address: {
-            id: 1202,
-            zip: 'JJH-982',
-            city: 'Denver'
-        },
-        getFullAdress(id) {
-            return this.address.city;
-        }
-    };
-})();
-(() => {
-    let addNumbersFunction;
-    addNumbersFunction = (a, b) => {
-        return 10;
-    };
-})();
+})(Validations || (Validations = {}));
+console.log(Validations.validateText('Jair'));
 //# sourceMappingURL=main.js.map
